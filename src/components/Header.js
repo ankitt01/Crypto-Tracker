4a224b5d9d75841fdae2e0c8e327611f332a9f13
ankitt01 <ankitrawat0102@gmail.com>
2022-03-10 21:31:53 +0530

    Created context

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import {useHistory} from "react-router-dom"
+import { CryptoState } from '../CryptoContext'
+
 
 const Header = () => {
   const history = useHistory()
+  const {currency, setCurrency} = CryptoState()
+  console.log(currency)
   return (
     <div className='flex justify-between mx-auto w-100 lg:w-3/5 p-4'>
         <h2 className='text-yellow-400 text-xl font-bold cursor-pointer' onClick={()=> history.push("/")}>Crypto Tracker</h2>
-        <select className='p-1.5 bg-inherit text-white border cursor-pointer'>
+        <select className='p-1.5 bg-inherit text-white border cursor-pointer' value={currency} onChange={(e) => setCurrency(e.target.value)}>
           <option className='bg-blackbg' value="INR">INR</option>
           <option className='bg-blackbg' value="USD">USD</option>
         </select>
